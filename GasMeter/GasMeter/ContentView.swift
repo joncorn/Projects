8eadc25163ad0287ec8e25693ba07f3816a45132
Jon Corn <jdcorn@live.com>
2022-05-08 20:21:13 -0700

    initial commit

---
+++ GasMeter/GasMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GasMeter
+//
+//  Created by Jon Corn on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GasMeter/GasMeter/GasMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasMeterApp.swift
+//  GasMeter
+//
+//  Created by Jon Corn on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GasMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
